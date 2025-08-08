ec9538846fbd28f44eae1e38a774f8f2e4826d6e
ZipperSpark <65334023+InnovoDeveloper@users.noreply.github.com>
2025-08-07 20:39:16 -0700

    again

--- components/sonic_i2c/sensor.py
+++ components/sonic_i2c/sensor.py
@@ -1,57 +1,40 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
-from esphome.components import sensor as sensor2, i2c
+from esphome.components import i2c, sensor
 from esphome.const import (
     CONF_ID,
-    CONF_TIMEOUT,
     STATE_CLASS_MEASUREMENT,
-    UNIT_METER, 
-    ICON_ARROW_EXPAND_VERTICAL,
+    UNIT_METER,
 )
 
-CONF_SDA_PIN = "sda_pin"
-CONF_SCL_PIN = "scl_pin"
-CONF_I2C_ADDR = 0x57
+DEPENDENCIES = ["i2c"]
 
-CONF_PULSE_TIME = "pulse_time"
-#MULTI_CONF = True
+# Create the C++ namespace and class
+sonic_i2c_ns = cg.esphome_ns.namespace("sonic_i2c")
+SonicI2CSensor = sonic_i2c_ns.class_(
+    "SonicI2CSensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
+)
 
-DEPENDENCIES = ['i2c']
-
-sonic_sensor_ns = cg.esphome_ns.namespace("sonic_i2c_sensor")
-SonicI2C = sonic_sensor_ns.class_("SonicI2C",sensor2.Sensor, i2c.I2CDevice, cg.PollingComponent)
-
+# Configuration schema - simplified
 CONFIG_SCHEMA = (
-    sensor2.sensor_schema(
-        SonicI2C,
+    sensor.sensor_schema(
+        SonicI2CSensor,
         unit_of_measurement=UNIT_METER,
-        icon=ICON_ARROW_EXPAND_VERTICAL,
-        accuracy_decimals=2,
+        accuracy_decimals=3,
         state_class=STATE_CLASS_MEASUREMENT,
     )
     .extend(
         {
-            #cv.Required(CONF_SDA_PIN): int,
-            #cv.Required(CONF_SCL_PIN): int,
+            cv.GenerateID(): cv.declare_id(SonicI2CSensor),
         }
     )
-    .extend(cv.polling_component_schema("60s")
-    .extend(i2c.i2c_device_schema(CONF_I2C_ADDR)))
+    .extend(cv.polling_component_schema("1s"))
+    .extend(i2c.i2c_device_schema(0x57))  # Default I2C address
 )
 
-
 async def to_code(config):
-    var = await sensor2.new_sensor(config)
+    """Generate the C++ code for the component"""
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    #var =  cg.new_Pvariable(config[CONF_ID])
-    #await cg.register_component(var, config)
-    #cg.add(var.set_sda_pin(config[CONF_SDA_PIN]))
-    #cg.add(var.set_scl_pin(config[CONF_SCL_PIN]))
+    await sensor.register_sensor(var, config)
     await i2c.register_i2c_device(var, config)
-    #cg.add(var.set_timeout_us(config[CONF_TIMEOUT] / (0.000343 / 2)))
-    #cg.add(var.set_pulse_time_us(config[CONF_PULSE_TIME]))
-
-    
-
-    
